865ab63a4bb712e3c8ebfa9805ac605beb663198
Noz0mu <100882770+Noz0mu@users.noreply.github.com>
2022-05-19 19:42:59 -0300

    Add files via upload

---
+++ Tower.js
@@ -0,0 +1,15 @@
+class Tower{
+constructor (x,y,w,h){
+let options = {
+isStatic: true,
+}
+this.w = w;
+this.h = h;
+this.body = Bodies.rectangle(x,y,this.w,this.h,options);
+World.add(world,this.body);
+}
+show(){
+    var pos = this.body.position
+    rect(pos.x,pos.y,this.w,this.h);
+}
+}
